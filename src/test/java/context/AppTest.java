62a3b1e8044c330c6a93259e836f79b6147b5522
Umut Ay Bora <umutaybora@gmail.com>
2024-11-25 15:05:01 +0100

    Unit tests and pipelines

--- src/test/java/context/AppTest.java
+++ src/test/java/context/AppTest.java
@@ -1,19 +1,38 @@
 package context;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import properties.PropertyUtilities;
+import java.util.logging.Logger;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest {
+class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
+    Logger log = Logger.getLogger(PropertyUtilities.class.getName());;
+
+    @BeforeEach
+    public void before(){ContextStore.loadProperties("test.properties");}
+
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void propertyReadingTest() {
+        Assertions.assertEquals("secret!", ContextStore.get("test-secret"), "Incorrect property value!");
+        log.info("propertyReadingTest() pass!");
+    }
+
+    @Test
+    public void defaultValueTest() {
+        Assertions.assertEquals("default-value", ContextStore.get("non-existent-property", "default-value"), "Default value mismatch!");
+        log.info("defaultValueTest() pass!");
+    }
+
+    @Test
+    public void loadValueTest() {
+        ContextStore.loadProperties("secret.properties");
+        Assertions.assertEquals("test-value-2", ContextStore.get("test-property"), "New did not load!");
+        log.info("loadValueTest() pass!");
     }
 }
